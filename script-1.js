d1037c6dfc852da238d9f87bb85dd0d9b3c8de7e
Erik Dubbelboer <erik@dubbelboer.com>
2011-04-13 13:11:44 +0200

    first commit

---
+++ script-1.js
@@ -0,0 +1,7 @@
+needs(['script-2.js'], 'script-1.js', function() {
+
+
+console.log('executing script-1.js');
+
+
+});
---
+++ script-3.js
@@ -0,0 +1,7 @@
+needs(['script-1.js', 'script-2.js'], 'script-3.js', function() {
+
+
+console.log('executing script-3.js');
+
+
+});
